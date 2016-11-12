bc257593d6320afd1e30470336cba6e282c47a34
Joel Low <joel@joelsplace.sg>
2016-11-12 18:52:34 +0800

    Use the configure and build scripts I wrote

--- win32/build.bat
+++ win32/build.bat
@@ -4,12 +4,17 @@ set basename=x64-msvcr120-ruby230
 
 set optflagsbase=/O2 /GL /Zo /Zi /favor:INTEL64 /arch:AVX
 set ldflagsbase=/incremental:no /debug /opt:ref /opt:icf
-set outdir=%~dp0usr
+set basedir=%~dp0
+set outdir=!basedir!usr
 
-@mkdir %~dp0build 2>nul
-cd %~dp0build
+@mkdir !basedir!build 2>nul
+cd !basedir!build
 
-if "%1" neq "" ( goto %1 )
+if "%1" neq "" (
+	set next=%1
+	shift
+	goto !next!
+)
 
 :link
 @rem for the extensions
@@ -20,12 +25,14 @@ del !basename!.dll
 @if not exist !basename!.pgd (
    call :doinstrument
 )
-nmake V=1 install test "OPTFLAGS=!optflagsbase!" "LDFLAGS=!ldflagsbase! /LTCG:PGUPDATE"
+nmake V=1 up install "OPTFLAGS=!optflagsbase!" "LDFLAGS=!ldflagsbase! /LTCG:PGUPDATE"
 @if errorlevel 1 ( exit %errorlevel% )
 mkdir !outdir!\lib\ruby\vendor_ruby\rubygems\defaults\
-copy %~dp0\rubygems_hooks.rb  !outdir!\lib\ruby\vendor_ruby\rubygems\defaults\operating_system.rb
+copy !basedir!\rubygems_hooks.rb  !outdir!\lib\ruby\vendor_ruby\rubygems\defaults\operating_system.rb
 @if errorlevel 1 ( exit %errorlevel% )
-signtool sign /a /t http://time.certum.pl !outdir!\bin\*.exe !outdir!\bin\!basename!.dll
+@if "%CI%" equ "" (
+	signtool sign /a /t http://time.certum.pl !outdir!\bin\*.exe !outdir!\bin\!basename!.dll
+)
 goto :eof
 
 :miniruby
@@ -62,7 +69,15 @@ del !basename!.pgc
 goto :link
 
 :configure
-call %~dp0configure --prefix=!outdir! --without-ext "dbm,gdbm,dl/callback,pty,readline,syslog,tk,tk/tkutil" --disable-install-doc --target=x64-mswin64
+set params=
+:configure_loop
+if [%1]==[] goto configure_afterloop
+set params=!params! %1
+shift
+goto configure_loop
+:configure_afterloop
+
+call !basedir!configure --prefix=!outdir! --without-ext "dbm,gdbm,dl/callback,pty,readline,syslog,tk,tk/tkutil" --disable-install-doc --target=x64-mswin64 !params!
 call :clean
 
 :clean
